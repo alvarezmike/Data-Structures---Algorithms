34c070e282f56c08dd2f43d4b428091b47787ba5
alvarezmike <kerer74@gmail.com>
2023-01-30 20:14:08 -0500

    Create O(n).py
    Created a Function to visualize O(n)

---
+++ 01- Big O/O(n).py
@@ -0,0 +1,6 @@
+def print_items (n):
+    """ O(n) function representaion, n equals the number of operations"""
+    for  i in range (n):
+        print(i)
+
+print_items(10) # in this case, O(n) = 10
