a06275c925dd87a3e6cea89d3616eda6ed4c1a15
alvarezmike <kerer74@gmail.com>
2023-02-08 19:48:32 -0500

    Done with DLL section
    completed different methods for doubly linked lists.

--- 04- DoublyLinkedLists/DoublyLinkedList.py
+++ 04- DoublyLinkedLists/DoublyLinkedList.py
@@ -50,10 +50,108 @@ class DoublyLinkedList:
         self.length -= 1
         return temp
 
+    def prepend(self, value):
+        """Method to add a new node to beginning (head)"""
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1
+        return True
+
+    def pop_first(self):
+        """Remove first node of list (head)"""
+        if self.length == 0:
+             return None
+        temp = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+            temp.next = None
+        self.length -= 1
+        return temp
+
+    def get(self, index):
+        """Get an item from a specific index"""
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        if index < self.length/2:
+            for _ in range(index):
+                temp = temp.next
+        else:
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev
+        return temp
+
+    def set_value(self, index, value):
+        """Add a a node at a specific index"""
+        temp = self.get(index)
+        if temp :
+            temp.value = value
+            return True
+        return False
+
+    def insert(self, index, value):
+        """Insert a new node in a specific index and value"""
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self. append(value)
+
+        new_node = Node(value)
+        before = self.get(index - 1)
+        after = before.next
+
+        new_node.prev = before
+        new_node.next = after
+        before.next = new_node
+        after.prev = new_node
+
+        self.length += 1
+        return True
+
+    def remove(self, index):
+        """Remove an item at an index"""
+        if index < 0 or index >= self.length:
+            return None
+        if index ==0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+
+        temp = self.get(index)
+
+        temp.next.prev = temp.prev
+        temp.prev.next = temp.next
+        temp.next = None
+        temp.prev = None
+
+        self.length-= 1
+        return temp
 
 
-
+# testing methods
 my_doubly_linked_list = DoublyLinkedList(1)
 my_doubly_linked_list.append(2)
+my_doubly_linked_list.append(9)
+my_doubly_linked_list.append(4)
+
+
 my_doubly_linked_list.print_list()
 
+my_doubly_linked_list.remove(1)
+
+print("after change")
+
+my_doubly_linked_list.print_list()
